36e774b95c6b7220b903f34323b96918cbaba18a
mahfujkhan02 <mahfujtanim450@gmail.com>
2024-02-27 16:15:37 +0600

    added navLink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './Header.css'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='header'>
 
             <nav>
-                <Link to='/home'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/products'>Products</Link>
-                <Link to='/post'>Posts</Link>
-                <Link to='/friends'>Friends</Link>
+                <NavLink className={({ isActive }) => isActive ? 'active' : undefined} to='/home'>Home</NavLink>
+                <NavLink to='/about'>About</NavLink>
+                <NavLink to='/products'>Products</NavLink>
+                <NavLink to='/post'>Posts</NavLink>
+                <NavLink to='/friends'>Friends</NavLink>
             </nav>
 
             <p>Common Header</p>
