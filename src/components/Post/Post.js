7f9e49a35c8bb7011614df00eea2f892b8160327
mahfujkhan02 <mahfujtanim450@gmail.com>
2024-02-27 15:44:14 +0600

    added button and link

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './Post.css'
+import { Link, useNavigate } from 'react-router-dom';
 
 const Post = ({post}) => {
-    const {title , body, userId} = post
+    const {id , title , body, userId} = post
+    const navigate = useNavigate()
+    const handleNavigate = () =>{
+
+        navigate(`/post/${id}`)
+
+    }
     return (
         <div className='post'>
             <p>ID : {userId}</p>
             <p>Title : {title}</p>
             <p>Body : {body}</p>
+            <Link to={`/post/${id}`}>visit : {id}</Link>
+            <Link to={`/post/${id}`}>
             <button>Show Details</button>
+            </Link>
+            <button onClick={handleNavigate}>Show Details 2</button>
         </div>
     );
 };
